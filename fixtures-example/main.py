995cf33b34562f9942cff986ece8e8dad568657d
Paul Khv <paul.khvaliuk@icloud.com>
2023-01-15 22:35:05 +0200

    json fixture example added

--- fixtures-example/main.py
+++ fixtures-example/main.py
@@ -2,6 +2,9 @@ import json
 
 if __name__ == "__main__":
     with open("fixtures.json", "r") as json_file:
-        fixture = json.load(json_file)
+        fixture: dict = json.load(json_file)
         print("Variable Type -> ", type(fixture))
-        print("Variable -> ", fixture)
+        for row in fixture.values():
+            print(f"Title: {row.get('title')}")
+            print(f"Author: {row.get('author')}")
+            print(f"Length: {row.get('length')}")
